ad63468bf3dfecca5cc466c9b4a633a2219226a9
ppooopppooo <ppooopppooo@qq.com>
2022-10-03 22:13:35 +0800

    pixel: remove DocumentUI from Google

--- pixel-vendor.mk
+++ pixel-vendor.mk
@@ -96,7 +96,6 @@ PRODUCT_COPY_FILES += \
     vendor/google/pixel/proprietary/system_ext/etc/permissions/privapp-permissions-google-se.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-google-se.xml
 
 PRODUCT_PACKAGES += \
-    DocumentsUIGoogle \
     GoogleExtServices \
     MarkupGoogle \
     PixelThemesStub \
